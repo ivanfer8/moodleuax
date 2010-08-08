3451773abce2d9bbcc8d2d64131b5119de8d4cf8
mevsmyself <mevsmyself@41540839-a5ac-330c-d833-4481519d0282>
2010-08-08 18:59:03 +0000

    [abel] - subida inicial

---
+++ AndMoodle/src/uax/android/moodle/android.java
@@ -0,0 +1,13 @@
+package uax.android.moodle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class android extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
