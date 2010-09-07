835ddb7fbc62dc3376c6d7b8d5a2d1a4eb345f8c
mevsmyself <mevsmyself@41540839-a5ac-330c-d833-4481519d0282>
2010-09-07 21:51:30 +0000

    abel - web service funcionando

--- AndMoodle/src/uax/android/moodle/android.java
+++ AndMoodle/src/uax/android/moodle/android.java
@@ -1,34 +1,30 @@
 package uax.android.moodle;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-import javax.ws.rs.core.MultivaluedMap;
-
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
@@ -36,108 +32,115 @@ import conv.android.moodle.MyUserConverter;
 import fac.android.moodle.User;
 
 public class android extends Activity {
-    /** Called when the activity is first created. */
-	
+	/** Called when the activity is first created. */
+
 	private Button mBuscar = null;
 	private EditText mTexto = null;
 	private final String DEBUG_TAG = "httpPostExample";
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.main);
-	    
-	    mBuscar = (Button)findViewById(R.id.buscar);
-		mTexto = (EditText)findViewById(R.id.token);
-		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mBuscar = (Button) findViewById(R.id.buscar);
+		mTexto = (EditText) findViewById(R.id.token);
+
 		mTexto.setTextColor(Color.BLUE);
 		mTexto.setText("4ae93a61942f8a07e5f738281807819a");
-		
+
 		mBuscar.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				ArrayList<User> usuario = (ArrayList<User>) listUserById(mTexto.getText().toString(), "moodle_user_get_users_by_id");
-				EditText nombreUsu = (EditText)findViewById(R.id.nombre);
-				if(!usuario.equals(null))
-					nombreUsu.setText(usuario.get(0).getName());
-				else
-				{
-					nombreUsu.setBackgroundColor(Color.RED);
-					nombreUsu.setText("Error");
+				try {
+					ArrayList<User> usuario = (ArrayList<User>) listUserById(
+							mTexto.getText().toString(),
+							"moodle_user_get_users_by_id");
+					EditText nombreUsu = (EditText) findViewById(R.id.nombre);
+					if (!usuario.equals(null))
+						nombreUsu.setText(usuario.get(0).getName());
+					else {
+						nombreUsu.setBackgroundColor(Color.RED);
+						nombreUsu.setText("Error");
+					}
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block e.printStackTrace();
 				}
 			}
 		});
-		
+
 	}
-	
-	public ArrayList<User> listUserById(String token, String moodleWebService){
-		//WebService list_user_By_Id
+
+	public ArrayList<User> listUserById(String token, String moodleWebService)
+			throws UnsupportedEncodingException {
+		// WebService list_user_By_Id
 		try {
-			Client client = Client.create();
-			
-			WebResource webResource = client.resource("http://192.168.1.34/moodle/webservice/rest/server.php");
 
-			MultivaluedMap<String, String> urlParams = new MultivaluedMapImpl();
-			urlParams.add("wstoken", token);
-			urlParams.add("wsfunction", moodleWebService);
+			HttpClient httpClient = new DefaultHttpClient();
 
-			MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
-			formData.add("userids[0]","1");
-			
-			//ClientResponse response = webResource.queryParams(urlParams).type("application/xml").post(ClientResponse.class, formData);
-			
-			HttpClient httpclient = new DefaultHttpClient(); 
-			HttpResponse response;
-            response = httpclient.execute( (HttpUriRequest) webResource.queryParams(urlParams).type("application/xml").post(ClientResponse.class, formData));
-			
-			
-			String xml = "";// response.getEntity(String.class);
-			
-			
-			
-			XStream xstream = new XStream(new DomDriver());
-			xstream.registerConverter(new MyUserConverter());
-			xstream.alias("RESPONSE",User.class);
+			HttpPost request = new HttpPost(
+					"http://192.168.1.34/moodle/webservice/rest/server.php?wstoken=4ae93a61942f8a07e5f738281807819a&wsfunction=moodle_user_get_users_by_id");
+			MultipartEntity entity = new MultipartEntity();
+			entity.addPart("userids[0]", new StringBody("1"));
+			request.setEntity(entity);
 
-			ArrayList<User> listUser = (ArrayList<User>) xstream.fromXML(xml);
+			String xml = "";
+			try {
+				HttpResponse response = httpClient.execute(request);
+				int status = response.getStatusLine().getStatusCode();
 
-			System.out.println(listUser.get(0).getName());
-			
-			return listUser;
-			
-		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			ResponseHandler<String> handler = new BasicResponseHandler();  
-			        	
-			HttpParams params = new BasicHttpParams();
-			
-			
-        	HttpClient httpclient = new DefaultHttpClient();  
-            HttpPost request = new HttpPost("http://192.168.1.34/moodle/webservice/rest/server.php");  
-            request.addHeader("wstoken", token);
-            request.addHeader("wsfunction", moodleWebService);
-//            request.setParams(data);
-            
-            HttpProtocolParams.setVersion( params, HttpVersion.HTTP_1_1);
-            HttpProtocolParams.setContentCharset( params, "UTF-8");
-            HttpProtocolParams.setUseExpectContinue( params, false);
-            
-            params.setParameter("userids[0]","1");
-            try {
-            	HttpResponse response;
-                response = httpclient.execute( request);
+				if (status != HttpStatus.SC_OK) {
+					ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+					response.getEntity().writeTo(ostream);
+					Log.e("HTTP CLIENT", ostream.toString());
+				} else {
+					InputStream content = response.getEntity().getContent();
+					// <consume response>
+					xml = readFromBuffer(new BufferedReader(
+							new InputStreamReader(content)));
+					content.close(); // this will also close the connection
+				}
 			} catch (ClientProtocolException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			}  
-			System.out.println("ERROR: "+e.getMessage());
+			}
+
+			XStream xstream = new XStream(new DomDriver());
+			xstream.registerConverter(new MyUserConverter());
+			xstream.alias("RESPONSE", User.class);
+
+			ArrayList<User> listUser = (ArrayList<User>) xstream.fromXML(xml);
+
+			System.out.println(listUser.get(0).getName());
+
+			return listUser;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+			System.out.println("ERROR: " + e.getMessage());
 			return null;
-		}		
+		}
+	}
+
+	private String readFromBuffer(BufferedReader br) {
+		StringBuilder text = new StringBuilder();
+		try {
+			String line;
+			while ((line = br.readLine()) != null) {
+				text.append(line);
+				text.append("\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			// tratar excepción!!!
+		}
+		return text.toString();
 	}
 
 }
